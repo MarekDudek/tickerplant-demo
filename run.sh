14ac1f1ae5c79061b639bb6aa9be688905e4535c
Marek Dudek <marek.dudek@gmail.com>
2021-03-16 22:31:41 +0100

    all ran

--- run.sh
+++ run.sh
@@ -4,6 +4,5 @@
 #P=~/q/start/tick/run1.sh
 P=./run1.sh
 
-#for f in ticker rdb hlcv last tq show vwap feed
-for f in ticker rdb hlcv last tq show vwap
+for f in ticker rdb hlcv last tq show vwap feed
 do $P $f; done
