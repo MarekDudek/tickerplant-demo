77d6ce15f5bb87f2bdc244bd40f4a90ef30d2375
Marek Dudek <marek.dudek@gmail.com>
2021-03-17 22:52:42 +0100

    script cleaned-up

--- run1.sh
+++ run1.sh
@@ -9,7 +9,7 @@ Q=/home/marek/Development/kdb/kdb4/l64/q
 
 # load each q process in a new terminal
 f() {
- gnome-terminal -t "$1" -e "$Q $2" &
+ gnome-terminal --title "$1" -- $Q $2 &
  sleep 0.25
 }
 
@@ -24,12 +24,12 @@ done
 
 
 case $1 in 
- "ticker" ) f "tickerplant" "tick.q -p 5010";w 5010 ;;
- "rdb" ) f $1 "tick/r.q -p 5011 -t 1000" ;;
- "hlcv" ) f $1 "$D/cx.q $1 -p 5014 -t 1000" ;;
- "last" ) f $1 "$D/cx.q $1 -p 5015 -t 1000" ;;
- "tq" ) f $1 "$D/cx.q $1 -p 5016 -t 1000" ;;
- "vwap" ) f $1 "$D/cx.q $1 -p 5017 -t 1000" ;;
- "show" ) f $1 "$D/cx.q $1" ;;
- "feed" ) f "feed" "$D/feed.q localhost:5010 -t 507" ;;
+ "ticker" ) f "tickerplant" "tick.q     -p 5010"     ; w 5010 ;;
+ "rdb" 	  ) f $1 	    "tick/r.q   -p 5011 -t 1000"      ;;
+ "hlcv"   ) f $1 	    "$D/cx.q $1 -p 5014 -t 1000"      ;;
+ "last"   ) f $1            "$D/cx.q $1 -p 5015 -t 1000"      ;;
+ "tq"     ) f $1            "$D/cx.q $1 -p 5016 -t 1000"      ;;
+ "vwap"   ) f $1            "$D/cx.q $1 -p 5017 -t 1000"      ;;
+ "show"   ) f $1            "$D/cx.q $1" 		      ;;
+ "feed"   ) f "feed"        "$D/feed.q localhost:5010 -t 507" ;;
 esac
